76047b5dd9434da0ee403baa24f6c54b303e8730
Samuel Painter <sam.foy.painter@gmail.com>
2012-06-29 16:49:30 -0400

    fixed missing semicolon

--- js/main.js
+++ js/main.js
@@ -275,7 +275,7 @@ $(document).ready(function() {
         e.layer.setStyle(defaultParcelStyle);
         console.log(e.properties.property_zoning_1_full);
         $('form#filter select').change(function() {
-            console.log($('select#zoning option:selected').val()
+            console.log($('select#zoning option:selected').val();
         });
 
         // Create a self-invoking function that passes in the layer
